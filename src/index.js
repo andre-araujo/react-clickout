6a3e3e460513c35c7dcd18f3b66d103795d120e9
aluis <andreluis9214@gmail.com>
2017-09-29 22:40:57 -0300

    feat: Add ClickOut component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-export default () => {};
+import React, { Component } from 'react';
+import { func, node } from 'prop-types';
+
+class ClickOut extends Component {
+    static propTypes = {
+        callback: func.isRequired,
+        children: node.isRequired,
+    }
+
+    static isOnClient = typeof window !== 'undefined'
+
+    componentDidMount() {
+        if (this.isOnClient) {
+            this.wrapper.addEventListener('click', this.onSelfClick);
+            document.addEventListener('click', this.onAnyClick);
+        }
+    }
+
+    componentWillUnmount() {
+        this.wrapper.removeEventListener('click', this.onSelfClick);
+        document.removeEventListener('click', this.onAnyClick);
+    }
+
+    onSelfClick = (e) => {
+        e.clickOutTarget = this.wrapper;
+    }
+
+    onAnyClick = (e) => {
+        const {
+            wrapper,
+            props: { callback },
+        } = this;
+
+        if (e.clickOutTarget !== wrapper) {
+            callback(e);
+        }
+    }
+
+    render() {
+        return (
+            <div ref={(wrapper) => { this.wrapper = wrapper; }}>
+                { this.props.children }
+            </div>
+        );
+    }
+}
+
+export default ClickOut;
